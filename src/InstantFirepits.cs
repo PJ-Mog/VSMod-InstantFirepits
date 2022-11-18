172d5f70cc69730feec4055c2ed4b5c45d052a91
PJ-Mog <JapanHasRice@gmail.com>
2022-11-18 10:30:32 -0500

    Fixes compatibility with VS v1.17.x versions.

--- src/InstantFirepits.cs
+++ src/InstantFirepits.cs
@@ -1,5 +1,4 @@
 using Vintagestory.API.Common;
-using Vintagestory.API.Server;
 using Vintagestory.API.Util;
 
 [assembly: ModInfo("Instant Firepits", "instantfirepits")]
@@ -9,19 +8,22 @@ namespace InstantFirepits {
     public override void Start(ICoreAPI api) {
       base.Start(api);
       api.RegisterBlockBehaviorClass("InstantFirepit", typeof(BehaviorInstantFirepit));
+    }
 
+    public override void AssetsFinalize(ICoreAPI api) {
       if (api.Side == EnumAppSide.Server) {
-        var sapi = api as ICoreServerAPI;
-        sapi.Event.ServerRunPhase(EnumServerRunPhase.ModsAndConfigReady, () => {
-          var stageOneFirepitBlock = sapi.World.BlockAccessor.GetBlock(new AssetLocation("firepit-construct1"));
-          if (stageOneFirepitBlock != null) {
-            stageOneFirepitBlock.BlockBehaviors = stageOneFirepitBlock.BlockBehaviors.Append(new BehaviorInstantFirepit(stageOneFirepitBlock));
-            sapi.Logger.Notification($"[InstantFirepits] Applied BehaviorInstantFirepit to {stageOneFirepitBlock.Code}");
-          }
-          else {
-            sapi.Logger.Warning("[InstantFirepits] Could not locate firepit block. No changes made.");
-          }
-        });
+        AddInstantFirepitBehavior(api);
+      }
+    }
+
+    public void AddInstantFirepitBehavior(ICoreAPI api) {
+      var stageOneFirepitBlock = api.World.BlockAccessor.GetBlock(new AssetLocation("firepit-construct1"));
+      if (stageOneFirepitBlock != null) {
+        stageOneFirepitBlock.BlockBehaviors = stageOneFirepitBlock.BlockBehaviors.Append(new BehaviorInstantFirepit(stageOneFirepitBlock));
+        api.Logger.Notification($"[InstantFirepits] Applied BehaviorInstantFirepit to {stageOneFirepitBlock.Code}");
+      }
+      else {
+        api.Logger.Warning("[InstantFirepits] Could not locate firepit block. No changes made.");
       }
     }
   }
